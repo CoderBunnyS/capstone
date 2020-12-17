39339726b68c8e31f6c62fe0a8d1356cca5fdbe6
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-12-17 10:53:35 -0800

    working version

--- preschool_django/settings.py
+++ preschool_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '_ho5^%sf2s8iose-(_efr3qx58ummj+gi5k=xkhqw+!$jiuf*4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['guarded-fjord-64430.herokuapp.com']
+ALLOWED_HOSTS = ['guarded-fjord-64430.herokuapp.com', 'localhost']
 
 
 # Application definition
