3e10df42a39e0d0f94ea11d8b7c1749f53457115
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-12-17 13:11:59 -0800

    attempting to solve css issue with deployed site

--- preschool_django/settings.py
+++ preschool_django/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
