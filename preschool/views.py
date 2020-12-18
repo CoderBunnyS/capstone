c2b6784ed01245952ee1731ba7fdb8d7df8de627
BUNNY_SCHAEFER <SCHAEFERBUNNY@GMAIL.COM>
2020-12-18 06:34:40 -0800

    working order

--- preschool/views.py
+++ preschool/views.py
@@ -79,9 +79,9 @@ class Login(View):
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        form = UserForm(request.POST)
-        if(form.is_valid()):
-            user = form.save()
+        # form = UserForm(request.POST)
+        # if(form.is_valid()):
+            # user = form.save()
             request.session['staff_logged_in'] = True
             return render('/', pk=user.pk)
             
